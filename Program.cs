9391e4f0d15b208b52ffe3fa626c74f99c692b52
Kitarsh <thomas.leclere@centrale-marseille.fr>
2022-02-24 21:47:01 +0100

    Try Quick BrutForce for all Inputs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿var fileName = "./../../../Inputs/a_an_example.in.txt";
-//var fileName = "./../../../Inputs/b_better_start_small.in.txt";
+﻿//var fileName = "./../../../Inputs/a_an_example.in.txt";
+var fileName = "./../../../Inputs/b_better_start_small.in.txt";
 //var fileName = "./../../../Inputs/c_collaboration.in.txt";
 //var fileName = "./../../../Inputs/d_dense_schedule.in.txt";
 //var fileName = "./../../../Inputs/e_exceptional_skills.in.txt";
@@ -10,12 +10,23 @@ InputService.SetInputFileName(fileName);
 var projects = InputService.GetProjects();
 Dictionary<int, Project> orderedProjects = new Dictionary<int, Project>();
 
-AssignementService.PopulateContributorsForProject(projects[1]);
-AssignementService.CompleteAllProjects(orderedProjects);
+//var temps = 0;
 
-AssignementService.PopulateContributorsForProject(projects[2]);
-AssignementService.CompleteAllProjects(orderedProjects);
-AssignementService.PopulateContributorsForProject(projects[0]);
-AssignementService.CompleteAllProjects(orderedProjects);
+//for (var i = 0; i < 1000; i++)
+//{
+//    // si un des projet est
+//    pr
+//    temps++
+//}
+for (int i = 0; i < projects.Count; i++)
+{
+    AssignementService.PopulateContributorsForProject(projects[i]);
+    AssignementService.CompleteAllProjects(orderedProjects);
+}
+
+//AssignementService.PopulateContributorsForProject(projects[2]);
+//AssignementService.CompleteAllProjects(orderedProjects);
+//AssignementService.PopulateContributorsForProject(projects[0]);
+//AssignementService.CompleteAllProjects(orderedProjects);
 
 OutputService.Write(orderedProjects);
